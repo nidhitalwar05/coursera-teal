ddaf42ab4bf17f137896c9dd88a477286b8e0048
Nidhi Talwar <nidhitalwar05@gmail.com>
2026-03-13 00:44:42 -0700

    Changes made to filters

--- app.R
+++ app.R
@@ -5,6 +5,10 @@ app <- teal::init(
     modules = example_module(
       label="Example Module"
     ),
+    filter= teal_slices(
+      teal_slice(dataname="IRIS", varname="Species", selected="setosa", fixed=TRUE),
+      teal_slice(dataname="IRIS", varname="Sepal.Length", anchored=TRUE)
+    ),
     header="My first teal app",
     footer="This is a test."
 )
